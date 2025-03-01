801b632953f3040c2ead8ba2ca3cce35fd779161
SpikeNew7774 <newadm159@gmail.com>
2025-03-01 19:47:25 +0100

    route.ts fix

--- Classes/Route.ts
+++ Classes/Route.ts
@@ -9,76 +9,80 @@ import type { Location } from "../Tools/Session.ts";
 import Session from "../Tools/Session.ts";
 import Page from "./Page.ts";
 
-
 /**
  * The Route class, which is used to create a global route.
  */
 class Route {
-    public readonly path: string;
+  public readonly path: string;
+  // deno-lint-ignore ban-types
+  public readonly handler: Function | Page;
+  private readonly Maid = new Maid();
+
+  public isActive: boolean = false;
+  public isMounted: boolean = false;
+
+  constructor({
+    path,
+    handler,
+  }: {
+    path: string;
     // deno-lint-ignore ban-types
-    public readonly handler: (Function | Page);
-    private readonly Maid = new Maid();
+    handler: Function | Page;
+  }) {
+    this.path = path;
+    this.handler = handler;
 
-    public isActive: boolean = false;
-    public isMounted: boolean = false;
+    // Bind the method to preserve 'this' context
+    this.onRouterNavigate = this.onRouterNavigate.bind(this);
 
-    constructor({
-        path,
-        handler
-    }: {
-        path: string,
-        // deno-lint-ignore ban-types
-        handler: (Function | Page)
-    }) {
-        this.path = path;
-        this.handler = handler;
+    Spicetify.Platform.History.listen(Session.RecordNavigation);
+    Session.RecordNavigation(Spicetify.Platform.History.location);
+  }
 
-        Spicetify.Platform.History.listen(Session.RecordNavigation);
-        Session.RecordNavigation(Spicetify.Platform.History.location);
-
-    }
-
-    private onRouterNavigate(data: Location) {
-        if (data.pathname === this.path) {
-            this.isActive = true;
-            if (this.handler instanceof Function) {
-                this.handler({ navigationData: data, isActive: this.isActive });
-            } else if (this.handler instanceof Page) {
-                if (!this.handler.isMounted) {
-                    this.handler.Mount();
-                }
-            }
-        } else {
-            this.isActive = false;
-            if (this.handler instanceof Page) {
-                if (this.handler.isMounted) {
-                    this.handler.Destroy();
-                }
-            }
+  private onRouterNavigate(data: Location) {
+    if (data.pathname === this.path) {
+      this.isActive = true;
+      if (this.handler instanceof Function) {
+        this.handler({ navigationData: data, isActive: this.isActive });
+      } else if (this.handler instanceof Page) {
+        if (!this.handler.isMounted) {
+          this.handler.Mount();
         }
-    }
-
-
-    public Mount() {
-        this.isMounted = true;
-        const routeNavigateListener = Event.listen("router:navigate", this.onRouterNavigate);
-
-        this.onRouterNavigate(Spicetify.Platform.History.location);
-
-        // Store a reference to the handler if it's a Page
-        if (this.handler instanceof Page) {
-            const pageHandler = this.handler;
-            this.Maid.Give(() => pageHandler.Destroy());
+      }
+    } else {
+      this.isActive = false;
+      if (this.handler instanceof Page) {
+        if (this.handler.isMounted) {
+          this.handler.Destroy();
         }
+      }
+    }
+  }
 
-        this.Maid.Give(() => Event.unListen(routeNavigateListener));
+  public Mount() {
+    this.isMounted = true;
+    const routeNavigateListener = Event.listen(
+      "router:navigate",
+      this.onRouterNavigate
+    );
+
+    // Check current location immediately after mounting
+    this.onRouterNavigate(Spicetify.Platform.History.location);
+
+    // Store a reference to the handler if it's a Page
+    if (this.handler instanceof Page) {
+      const pageHandler = this.handler;
+      this.Maid.Give(() => pageHandler.Destroy());
     }
-    
-    public Destroy() {
-        this.isActive = false;
-        this.isMounted = false;
-        this.Maid.Destroy();
-    }
+
+    this.Maid.Give(() => Event.unListen(routeNavigateListener));
+  }
+
+  public Destroy() {
+    this.isActive = false;
+    this.isMounted = false;
+    this.Maid.Destroy();
+  }
 }
 
 export default Route;
